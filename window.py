2e4e7ad19d6598319e6d7004c8078417cadde28c
codeAB <631706073@qq.com>
2014-12-22 13:01:31 +0800

    modify

---
+++ window.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python
+#coding=utf-8
+
+import sys,os
+from PyQt4.QtGui import *
+from PyQt4.QtCore impoer *
+
+class window(QWidget):
+	def __init__(self):
+		super(window,self).__init__()
+
+
+
+
+if __name__ == "__main__":
+	print "请执行run.py"
+	return
