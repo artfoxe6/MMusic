468b2101f12b06ebfb4ab8072194ba702dacb2e6
codeAB <631706073@qq.com>
2014-12-22 23:08:28 +0800

    modify

---
+++ myClass.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+"""
+为了更加灵活的扩展和修改程序逻辑,
+我不直接使用一部分Qt的组件,而是去继承这些组件封装一些自己的逻辑
+所以你在其他文件中看到一些莫名其妙的名称的组件,请到这里面查找
+"""
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+#播放器外框
+class windowWidget(QWidget):
+	def __init__(self):
+		super(windowWidget,self).__init__()
+
+# 播放器头部Widget
+class headWidget(QWidget):
+	def __init__(self,window):
+		super(headWidget,self).__init__()
+		self.window = window
+	#重写三个方法使头部支持拖动,上面参数window就是拖动对象
+	def mousePressEvent(self, event):
+		if event.button()==Qt.LeftButton:
+			self.m_drag=True
+			self.window.m_DragPosition=event.globalPos()-self.window.pos()
+			event.accept()
+	def mouseMoveEvent(self, QMouseEvent):
+		if QMouseEvent.buttons() and Qt.LeftButton and self.m_drag:
+			self.window.move(QMouseEvent.globalPos()-self.window.m_DragPosition)
+			QMouseEvent.accept()
+	def mouseReleaseEvent(self, QMouseEvent):
+		self.m_drag=False
+
+
+
--- window.py
+++ window.py
@@ -4,51 +4,84 @@
 import sys,os
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+from myClass import *
 
-# -----------------------自定义myWidget类，实现鼠标拖动-------------------
-#这个自定义myWidget里面必须有自节点，否则无法定义样式，参见下面的测试代码
-class myWidget(QWidget):
-	def __init__(self,window):   #window参数是要被移动的窗口
-		super(myWidget,self).__init__()
-		self.window = window
-	def mousePressEvent(self, event):
-		if event.button()==Qt.LeftButton:
-			self.m_drag=True
-			self.window.m_DragPosition=event.globalPos()-self.window.pos()
-			print event.globalPos()
-			print self.window.pos()
-			event.accept()
-	def mouseMoveEvent(self, QMouseEvent):
-		if QMouseEvent.buttons() and Qt.LeftButton and self.m_drag:
-			self.window.move(QMouseEvent.globalPos()-self.window.m_DragPosition)
-			QMouseEvent.accept()
-	def mouseReleaseEvent(self, QMouseEvent):
-		self.m_drag=False
-
-# ----------------测试-------------------
-class window(QWidget):
+class main(QWidget):
 	def __init__(self):
-		super(window,self).__init__()
-		self.resize(200,200)
+		super(main,self).__init__()
+		# self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+		self.setGeometry(300, 30, 300,600)
 		self.dropWd()
-		self.staticWd()
+		self.addLayout()
+	def  addLayout(self):
+		# ==========================头部=================================
+		sp = headWidget(self)   #这里的self就是被移动的窗口
+		sp.setParent (self)  #这里的self是所属父级
+		sp.setStyleSheet("QWidget{background:#7FAEE4}QLabel{color:white}\
+			QPushButton{border:none;color:white}QPushButton:hover{color:blue}")
+		sp.setGeometry(0,0,300,200)
+		QLabel(sp).resize(300,120)  #这个自定义myWidget里面必须有东西 不能为空，否则无法定义样式
+		# label.resize(300,120)
+		# ----------------头部内部组件-------------------------
+		QLabel(u"~梦音乐v1.0",sp) .setGeometry(10,0,100,20)
+		QPushButton(u"关闭",sp) .setGeometry(260,0,30,20)
+		QPushButton(u"隐藏",sp) .setGeometry(220,0,30,20)
+		# ------歌手头像
+		songerPic = QLabel(sp)
+		songerPic.setGeometry(10,20,72,72)
+		img = QImage("src/songer.png")
+		img = img.scaled(70,120,Qt.KeepAspectRatio)
+		songerPic.setPixmap(QPixmap.fromImage(img))
+		# ------当前播放的歌名
+		QLabel(u"我可以抱你吗-张惠妹",sp).setGeometry(110,20,200,20)
+		#-------上一曲
+		preSong = QLabel(sp) 
+		preSong.setGeometry(120,60,24,24)
+		img = QImage("src/pre.png")
+		img = img.scaled(24,24,Qt.KeepAspectRatio)
+		preSong.setPixmap(QPixmap.fromImage(img))
+		#--------播放
+		preSong = QLabel(sp) 
+		preSong.setGeometry(160,48,50,50)
+		img = QImage("src/play.png")
+		img = img.scaled(48,48,Qt.KeepAspectRatio)
+		preSong.setPixmap(QPixmap.fromImage(img))
+		#--------下一曲
+		preSong = QLabel(sp) 
+		preSong.setGeometry(225,60,24,24)
+		img = QImage("src/next.png")
+		img = img.scaled(24,24,Qt.KeepAspectRatio)
+		preSong.setPixmap(QPixmap.fromImage(img))
+		# ===========================滚动条==============================
+		proWgt = QWidget(self)
+		proWgt.setGeometry(0, 110, 300,10)
+		proWgt.setStyleSheet("QWidget{background-color:#C9C9C9;border-top:none;border-bottom:none}\
+		    SeekSlider{width:300}")
+		# ===========================歌曲列表==============================
+		listWgt = QWidget(self)
+		listWgt.setGeometry(0, 120, 300,440)
+		listWgt.setStyleSheet("QWidget{color:white;background:white;border-top:none;border-bottom:none}")
+		songList = QListWidget(listWgt)
+		songList.resize(260,440)   
+		songList.setStyleSheet("QListWidget{color:gray;font-size:12px;background:#FAFAFD;}\
+		    QScrollBar{width:0;height:0}\
+		    ")
+		# =============================底部=============================
+		footWgt = QWidget(self)
+		footWgt.setGeometry(0, 560, 300,40)
+		footWgt.setStyleSheet("QWidget{color:white;background:#7FAEE4;border-top:none;}\
+		QPushButton{background:none;color:white;}\
+		QPushButton:hover{background:none;color:blue;}\
+		")
 	def dropWd(self):
 		dp = QWidget(self);
 		dp.setGeometry(150,150,50,50)
 		dp.setStyleSheet("QWidget{background:red}")
 		dp.show()
-	def staticWd(self):
-		sp = myWidget(self)   #这里的self就是被移动的窗口
-		sp.setParent (self)  #这里的self是所属父级
-		sp.setStyleSheet("QWidget{background:blue}")
-		sp.resize(50,50)
-		label = QLabel("haha",sp)  #这个自定义myWidget里面必须有东西 不能为空，否则无法定义样式
-		label.resize(50,50)
-		label.show()
-		sp.show()
+		
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
-	demo = window()
+	demo = main()
 	demo.show()
 	sys.exit(app.exec_())
